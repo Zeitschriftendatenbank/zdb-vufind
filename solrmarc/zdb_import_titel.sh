67fae1ffb6f4b0cbfba30f50aa14c79d463f9f0a
cKlee <carsten.klee@sbb.spk-berlin.de>
2013-03-08 10:46:22 +0100

    solrmarc properties for exemplar

--- solrmarc/zdb_import_titel.sh
+++ solrmarc/zdb_import_titel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RUN_CMD ="java -Xms512m -Xmx512m -Dsolrmarc.solr.war.path=/var/lib/jetty/webapps/solr.war -Dsolr.core.name=biblio -Dsolrmarc.path=/usr/local/vufind/solrmarc -Dsolr.path=/usr/local/vufind/solr-4.1.0 -jar /usr/local/vufind/solrmarc/SolrMarc.jar vufind_titel_config.properties /home/zdb/data/samples_titles.mrc"
+RUN_CMD="java -Xms512m -Xmx512m -Dsolrmarc.solr.war.path=/var/lib/jetty/webapps/solr.war -Dsolr.core.name=biblio -Dsolrmarc.path=/usr/local/vufind/solrmarc -Dsolr.path=/usr/local/vufind/solr-4.1.0 -jar /usr/local/vufind/solrmarc/SolrMarc.jar vufind_titel_config.properties /home/zdb/data/samples_titles.mrc"
 ERROR_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-error-log"
 IMPORT_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-import-log"
 
