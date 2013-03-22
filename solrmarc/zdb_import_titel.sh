a1eaaa2f9f6db289882f28a0aa4ac4a023a0b9d1
cKlee <carsten.klee@sbb.spk-berlin.de>
2013-03-22 14:11:14 +0100

    ending project commits

--- solrmarc/zdb_import_titel.sh
+++ solrmarc/zdb_import_titel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-RUN_CMD="java -Xms512m -Xmx512m -Dsolrmarc.solr.war.path=/var/lib/jetty/webapps/solr.war -Dsolr.core.name=biblio -Dsolrmarc.path=/usr/local/vufind/solrmarc -Dsolr.path=/usr/local/vufind/solr-4.1.0 -jar /usr/local/vufind/solrmarc/SolrMarc.jar vufind_titel_config.properties /home/zdb/data/samples_titles.mrc"
-ERROR_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-error-log"
-IMPORT_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-import-log"
+RUN_CMD="java -server -Xms512m -Xmx512m -Dsolrmarc.solr.war.path=/var/lib/jetty/webapps/solr.war -Dsolr.core.name=biblio -Dsolrmarc.path=/usr/local/vufind/solrmarc -Dsolr.path=/usr/local/vufind/solr-4.1.0 -jar /usr/local/vufind/solrmarc/SolrMarc.jar vufind_titel_config.properties /home/zdb/data/zdbtitel1302vorutf8.mrc"
+ERROR_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-error-log_titles"
+IMPORT_LOG="/usr/local/vufind/solr-4.1.0/logs/solrmarc-import-log_titles"
 
 exec > $IMPORT_LOG
 exec 2> $ERROR_LOG
